502bb754ad48dfae3d64a3c158acf88c13a2f8ed
NightWing2110 <luuquanghoangdy@gmail.com>
2021-12-24 09:17:52 +0700

    develop

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Alo Idol!',
+    introDesc: `Merry Chirstmas🎄🎄🎄`,
+    btnIntro: 'Merry Chirstmas',
+    title: 'Đi chơi noel ko :v',
+    desc: '☃️☃️☃️',
+    btnYes: 'Oki🍺 ',
+    btnNo: 'Không bao h:3',
+    question: 'Ưng đi đâu?',
+    btnReply: 'Send',
+    reply: 'Bún đậu mắm tômmmmmmmmmmm',
+    mess: 'Oki chốt đơn :v',
+    messDesc: '',
+    btnAccept: 'Okiiiii',
+    messLink: 'https://www.facebook.com/messages/t/100033961174820'
+}
